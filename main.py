ae9c55aca8efd545c0cab95ae9af3e060aa0794c
Gerry Della Rocca <gerry.della.rocca@gmail.com>
2023-11-11 12:07:57 -0500

    Updates to the infrastructure. Generation of the problem statement.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #Final Project Main Run Function
 
+import os
 import time
 import matplotlib.pyplot as plt  
-import pandas as pd
 import numpy as np
-from copy import deepcopy
 import pickle
+from utils import generate_floor_mask,generate_transition_matrix
 
 
 #Main Function
@@ -25,16 +25,116 @@ def main():
     X_max=3 #Length of the X grid in dimensionless distance
     Y_max=1 #Length of the Y grid in dimensionless distance
 
-    #Sea Floor Input - .csv file that follows the sea floor: both X and Y were normalized to 1 by their respective max distances
-    
+    #Starting location i,j for the AUV
+    start_location=[1,n_y-2]
 
-    #Action Space - 4 possible actions since we assume we always want to be trying to head towards the goal
-    # 0 - go in the +x direction
-    # 1 - go in the -x direction
-    # 2 - go in the +y direction
-    # 3 - go in the -y direction
+    #End Location - only i is provided, j will be determined as just above the sea floor
+    end_location_i=225
+
+    #Sea Floor Input - .csv file that follows the sea floor: both X and Y were normalized to 1 by their respective max distances
+    #Generated from a drawn curve using https://plotdigitizer.com/app to digitize it.
+    sea_floor_file='seafloor_data.csv'
+
+    #Action Space - 4 possible actions since we assume we always want to be trying to head towards the goal - affects the mean of the distribution we would draw our next state from
+    # 0 - go in the +x direction 1 unit i
+    # 1 - go in the -x direction 1 unit i
+    # 2 - go in the +y direction 1 unit j
+    # 3 - go in the -y direction 1 unit j
+    # action offsets captures these as action[a,0] is the i_offset, action[a,1] is the j offset
     action_max=3
-        
+    action_offsets=np.zeros((action_max+1,2))
+    action_offsets[0,:]=[1,0]
+    action_offsets[1,:]=[-1,0]
+    action_offsets[2,:]=[0,1]
+    action_offsets[3,:]=[0,-1]
+
+    #Reward Matrix - assume this is only a function of the state s, with a reward only at reaching our goal
+    #Default reward is 0
+    # R(i,j) corresponds to the reward at state i,j
+    reward_goal=100
+    R=np.zeros((n_x,n_y))
+
+    #Transition Matrix 
+    #T(i,j,k,7,7) - Transition matrix probabilities for starting state (i,j), action k, and the other components are the probability of ending up in that neigbhoring state - offsets centered at 
+    # the point i,j
+    #Normal distributions pdf are used to assign a probability to each cell and the results are normalized afterwards. This does not account for any exterme transitions in probability, but those are unlikely
+    #Also this approach avoids having to try to calculate the 2D CDF to figure out the exat probability we are in a box.
+    #Any transition that would go outside of the (i,j) domain taking that action will assign its probability to the nearest cell in the domain. Any transition that would take us into a wall we instead assign it to staying put
+    transition_offsets=np.array([-3,-2,-1,0,1,2,3])
+    T=np.zeros((n_x,n_y,action_max,7,7))
+
+    #Multivariate normal offset parameters - magnitude scales linearly at each i location from 0 at the floor location in y up the maximums that are here at the surface
+    #Ignoring the fact that the constant of at the surface violates the conservation of mass for now
+    T_stat_mean=np.array([0,0]) #Mean offset at the surface [2,0] will be +2 offset in the x-direction
+    T_stat_covariance=np.array([[1,0],[0,1]]) #Mean standard deviation for the covariance strength for nudging in various directions
+
+    #Policy storage matrix P[i,j]=a means take action a at location P[i,j] according to the policy
+    #Just initializing as an output example
+    P=np.zeros((n_x,n_y))
+
+    #Rerun or used saved generation of the domain
+    regenerate_flag=True
+    domain_output_file='Saved_Data/geometry.pkl'
+
+    #########################################################
+    #Group 2: Policy Evaluation Inputs
+
+    #Evaluation Routine
+    # 1 - Value Iteration with Gauss Seidel based on distance to goal
+    policy_eval_choice=1
+
+    ########################
+    # Choice 1 Input Parameters
+
+    ##########################################################
+    #Group 3: Metric Evaluation + Visualizations
+    #Log File Name
+    log_file_name='local_log.csv'
+    #Run Name for this run
+    run_name='test'
+    #Number of simulation attempts for our policy and the default policies
+    n_sims=100
+    #Maximum simulation number of steps to try to get to goal - something goes very wrong if we hit this
+    sim_max=1000
+
+    #############################################################################################################################################################################
+    #Actual Code to run
+
+    #Group 1: Domain generation
+    if regenerate_flag:
+        #Read in the floor of the ocean and generate a mask file that is 1 where the ocean floor is in our discretized domain and 0 otherwise
+        floor_mask=generate_floor_mask(sea_floor_file,n_x,n_y)
+
+        #Determine the final location point and create a terminal mask - terminal is 0 when it is not an endpoint and 1 when it is an endpoint
+        ind=np.argmin(floor_mask[end_location_i,:])
+        terminal_mask=np.zeros((n_x,n_y))
+        terminal_mask[end_location_i,ind]=1
+        #Set the reward matrix for the end point to the appropriate value
+        R[end_location_i,ind]=reward_goal
+        end_location=[end_location_i,ind]
+
+        #Generate the transition matrix for the problem
+        T=generate_transition_matrix(T,transition_offsets,action_offsets,T_stat_mean,T_stat_covariance,floor_mask,terminal_mask)
+
+        #Write out the generated results
+        with open(domain_output_file, "wb") as fp:
+            pickle.dump((R, T, floor_mask, terminal_mask,end_location), fp)
+    else:
+        #Reload previously generated information
+        #Load the Value iteration variables
+        with open(domain_output_file, "rb") as fp:
+            temp = pickle.load(fp)
+        R=temp[0]
+        T=temp[1]
+        floor_mask=temp[2]
+        terminal_mask=temp[3]
+        end_location=temp[4]
+        breakpoint()
+
+    # #Plot of the domain results
+    # plt.imshow(floor_mask.T, origin="lower",extent=[0,X_max,0,Y_max])
+    # plt.colorbar()
+    # plt.show()
     print('Building')
 
 
--- utils.py
+++ utils.py
@@ -1,20 +1,156 @@
+#!/usr/bin/env python3
 import math, pdb, os, sys
 
 import numpy as np, tensorflow as tf, matplotlib.pyplot as plt
+import pandas as pd
+from scipy import interpolate
+from scipy.stats import multivariate_normal
+
+#Function to generate the mask for the ocean floor
+def generate_floor_mask(sea_floor_file,n_x,n_y):
+    #Inputs:
+    # sea_floor_file - .csv file for the sea floor location in normalized x,y coordinates
+    # n_x - number of points in the x direction
+    # n_y -number of points in the y-direction
+    #
+    # Output:
+    # mask_matrix - n_x x n_y mask matrix for the sea floor - 0 is open domain, 1 is where the ocean floor is
+
+    #Read in the sea floor file
+    sea_floor=pd.read_csv(sea_floor_file)
+    sea_floor=np.array(sea_floor)
+
+    #X-locations for interpolation of the floor
+    x_locations=np.linspace(0,n_x-1,n_x)/(n_x-1)
+    f=interpolate.interp1d(sea_floor[:,0],sea_floor[:,1],bounds_error=False,fill_value="extrapolate")
+    j_floor=np.ceil(f(x_locations)*(n_y-1)).astype(dtype='int32')
+    #Matrix for the mask 
+    mask=np.zeros((n_x,n_y))
+    for i in range(n_x):
+        mask[i,0:j_floor[i]]=1
+    return mask
+
+#Function to generate the Transition Matrix
+def generate_transition_matrix(T,transition_offsets,action_offsets,T_stat_mean,T_stat_covariance,floor_mask,terminal_mask):
+    #Inputs:
+    # T - original transition matrix variable - T(i,j,a,n,n) where i is the x-coordinate, j is the y-coordiante, a is the action index, and nxn is the matrix of points we could transition to
+    # transition_offsets - n vector that has the offsets for our transition nxn matrix points from the starting point of interest - ideally this is centered on our point. Must be monotonically increasing
+    # action_offsets - ideal i,j offsets an action will take where [a,0] = the i offset, [a,1] = the j offset
+    # T_stat_mean - multivariant normal distribution mean values for offset at the maximum of the top of the domain
+    # T_stat_covariance - multivariat normal distribution covariances at the maximum at the top of the domain
+    # floor_mask - Matrix =1 if this is a floor point, 0 otherwise
+    # terminal_mask -Matrix = 1 if this is an endpoint, 0 otherwise
+    #Outputs:
+    # T - Transition Matrix T
+
+    #Notes:
+    #Multivariate normal offset parameters - magnitude scales linearly at each i location from 0 at the floor location in y up the maximums that are here at the surface
+    #All Floor points and terminal points remain in their state regardless of the action chosen
+    
+    #########################################################################################################
+
+    #Get the domain sizes
+    (n_x,n_y,n_a,n1,n2)=T.shape
+    #Note there is an assumption that n1 and n2 are the same given the transition_offsets being a 1D vector - could expand this out if needed.
+
+    #Assumed that the transition offsets includes 0 and includes it only once - throw an error if otherwise    
+    if np.sum(transition_offsets==0)!=1:
+        print('generate_transition_matrix: Error the number of transition offsets is wrong.')
+        exit()
+
+    #Determine minimum j value that is in the ocean domain for each i value
+    j_min=np.zeros(n_x)
+    for i in range(n_x):
+        #Determine location of the floor
+        j_min[i]=np.argmin(floor_mask[i,:]).astype('int32')
+
+    #Loop over all states and actions to fill in the appropriate transition matrix
+    for i in range(n_x):
+        print('Generating Transition Matrixices ' +str(i+1) + ' of '+ str(n_x))
+        for j in range (n_y):
+            for k in range(n_a):
+                #Reset my transition matrix to all zeros for now
+                T_temp=np.zeros((n1,n2))
+                #Check if we are a terminal point or an masked point
+                if floor_mask[i,j]==1 or terminal_mask[i,j]==1:
+                    #Set to just staying in place
+                    ind=np.where(transition_offsets==0)
+                    T_temp[ind,ind]=1
+                    T[i,j,k,:,:]=T_temp
+                    continue
+                else:
+                    #Compute the multivariate distribution for this point without accounting for the edges of the domains or walls
+                    #Compute the scale factor based on our location relative to the top
+                    scale_factor=(j-j_min[i])/(n_y-1-j_min[i])
+                    #Mean offset based on the action and our location
+                    mean_offset=T_stat_mean*scale_factor+action_offsets[k,:].squeeze()
+                    #Covariance scaling- add small scaling to avoid zero problem at the bottom
+                    covar=T_stat_covariance*(scale_factor+sys.float_info.epsilon)
+                    #Loop using the Multivariant normal distribution to fill in our cases - using a coarse approximation here that the value of the pdf at this point is 
+                    # representative of the relative weighting for that cell and then normalizing 
+                    mvn=multivariate_normal(mean=mean_offset,cov=covar,allow_singular=False)
+                    for ii in range(n1):
+                        for jj in range(n2):
+                            #Compute the multivariant PDF for this point
+                            T_temp[ii,jj]=mvn.pdf([transition_offsets[ii],transition_offsets[jj]])
+                    #Normalize the probability results
+                    T_temp=T_temp/np.sum(T_temp)
+                    #Loop to reassign probabilities of any points that are outside of the domain to the nearest point in the domain - for the edges, this will always be the one 
+                    #that is in the opposite direction we went out of bounds
+                    #Entering a wall instead assign that we don't move
+                    #Need to do these one at a time unforunately to avoid a possible conflicts with reassigning a probability in multiple places
+                    #Lower bound i case
+                    if i<np.abs(np.min(transition_offsets))+1:
+                        #Update the out of bounds in x cases
+                        for ii in range(n1):
+                            if i+transition_offsets[ii]<0:
+                                #Rolling addition over
+                                T_temp[ii+1,:]+=T_temp[ii,:]
+                                T_temp[ii,:]=0
+                    #Upper bound i case
+                    if i>np.abs(np.max(transition_offsets))-1:
+                        #Update the out of bounds in x cases
+                        for ii in reversed(range(n1)):
+                            if i+transition_offsets[ii]>n_x-1:
+                                #Rolling addition over
+                                T_temp[ii-1,:]+=T_temp[ii,:]
+                                T_temp[ii,:]=0 
+                    #Lower bound j case
+                    if j<np.abs(np.min(transition_offsets))+1:
+                        #Update the out of bounds in y cases
+                        for jj in range(n2):
+                            if j+transition_offsets[jj]<0:
+                                #Rolling addition over
+                                T_temp[:,jj+1,:]+=T_temp[:,jj]
+                                T_temp[:,jj]=0
+                    #Upper bound i case
+                    if j>np.abs(np.max(transition_offsets))-1:
+                        #Update the out of bounds in y cases
+                        for jj in reversed(range(n2)):
+                            if j+transition_offsets[jj]>n_y-1:
+                                #Rolling addition over
+                                T_temp[:,jj-1]+=T_temp[:,jj]
+                                T_temp[:,jj]=0  
+                    #Wall assignments
+                    #no movement point location
+                    ind_no_move=np.where(transition_offsets==0)
+                    for ii in range(n1):
+                        for jj in range(n2):
+                            #see if this point is in a wall
+                            ind1=i+transition_offsets[ii]
+                            ind2=j+transition_offsets[jj]
+                            if ind1>=0 and ind2>=0 and ind1<n_x and ind2<n_y:
+                                if floor_mask[ind1,ind2]==1:
+                                    #Reassign this probability to no movement
+                                    T_temp[ind_no_move,ind_no_move]+=T_temp[ii,jj]
+                                    T_temp[ii,jj]=0
+                    T[i,j,k,:,:]=T_temp
+
+    return T
 
 
-def map_chunked(fn, chunk_size, n, verbose=False):
-    """
-    Map a function over iterates in chunks to save memory.
-    You DO NOT need to use this.
-    """
-    ret = []
-    rng = range(math.ceil(n / chunk_size))
-    rng = rng if not verbose else tqdm(rng)
-    for i in rng:
-        i1, i2 = i * chunk_size, min((i + 1) * chunk_size, n)
-        ret.append(fn(i1, i2))
-    return tf.concat(ret, 0)
+###########################################################################################################
+##### Old Functions from the old projects
 
 
 def visualize_value_function(V):
